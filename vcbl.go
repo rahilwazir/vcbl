6ad29604a4950e7ebd54c97111b02c364fca7d8f
rahilwazir <rahilwazir@hotmail.com>
2018-01-17 07:41:48 +0500

    Capitalize lookup words #1

--- vcbl.go
+++ vcbl.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/urfave/cli"
@@ -12,6 +13,70 @@ import (
 const (
 	url = "https://www.vocabulary.com/dictionary/definition.ajax?search=%s"
 )
+
+var (
+	retry = 0
+)
+
+func getDefinition(c *cli.Context, lookup string) {
+	doc, err := goquery.NewDocument(fmt.Sprintf(url, lookup))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if doc.Find("div.noresults").Length() == 1 {
+		fmt.Println("No results.")
+
+		if retry == 0 {
+			lookup = strings.Title(lookup)
+			fmt.Printf("Trying %q...", lookup)
+			getDefinition(c, lookup)
+			retry++
+		}
+
+		return
+	}
+
+	shortDesc := doc.Find("p.short").Text()
+	longDesc := doc.Find("p.long").Text()
+
+	text := fmt.Sprintln()
+
+	switch c.String("desc") {
+	case "long":
+		text += fmt.Sprintln(longDesc)
+	case "both":
+		text += fmt.Sprintln(shortDesc)
+		text += fmt.Sprintln("------------------")
+		text += fmt.Sprintln(longDesc)
+	default:
+		text += fmt.Sprintln(shortDesc)
+	}
+
+	if doc.Find("table.definitionNavigator").Length() != 0 {
+		doc.Find("table.definitionNavigator tr").Each(func(i int, tr *goquery.Selection) {
+			groupNumber := tr.Find(".groupNumber").Text()
+			def := tr.Find(".def").Text()
+
+			text += fmt.Sprintf("%s %s\n", groupNumber, def)
+		})
+	} else {
+		doc.Find("div.ordinal").Each(func(i int, ordinal *goquery.Selection) {
+			if i == 0 {
+				groupNumber := ordinal.Closest("div.group").Find(".groupNumber").Text()
+				text += fmt.Sprintf("%s.\n", groupNumber)
+			}
+
+			wordType := ordinal.Find("h3.definition").Find(".anchor").Text()
+			text += fmt.Sprintf("(%s) ", wordType)
+
+			h3Definition := ordinal.Find("h3.definition").First().Contents().Eq(2).Text()
+			text += fmt.Sprintf("%s\n", strings.TrimSpace(h3Definition))
+		})
+	}
+
+	fmt.Print(text)
+}
 
 func main() {
 	app := cli.NewApp()
@@ -36,38 +101,4 @@ func main() {
 		return nil
 	}
 	app.Run(os.Args)
-}
-
-func getDefinition(c *cli.Context, lookup string) {
-	doc, err := goquery.NewDocument(fmt.Sprintf(url, lookup))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if doc.Find("div.noresults").Length() == 1 {
-		fmt.Println("No results.")
-		return
-	}
-
-	shortDesc := doc.Find("p.short").Text()
-	longDesc := doc.Find("p.long").Text()
-
-	text := fmt.Sprintln()
-
-	switch c.String("desc") {
-	case "long":
-		text += fmt.Sprintln(longDesc)
-	case "both":
-		text += fmt.Sprintln(shortDesc)
-		text += fmt.Sprintln("------------------")
-		text += fmt.Sprintln(longDesc)
-	default:
-		text += fmt.Sprintln(shortDesc)
-	}
-
-	doc.Find("table.definitionNavigator tr").Each(func(i int, tr *goquery.Selection) {
-		text += fmt.Sprintf("%s %s\n", tr.Find(".groupNumber").Text(), tr.Find(".def").Text())
-	})
-
-	fmt.Print(text)
 }
